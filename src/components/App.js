9b315531df543ebf36fb473a46fb5a492285a078
quentin dupuy <quentin.dupuy69@gmail.com>
2021-05-31 16:04:19 +0200

    Added AddTask Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './NavBar';
 import ToDoList from './ToDoList';
+import AddTask from './AddTask';
 
 class App extends React.Component{
     render(){
         return(
             <section>
                 <ToDoList />
+                <AddTask />
                 <NavBar />
             </section>
         )
